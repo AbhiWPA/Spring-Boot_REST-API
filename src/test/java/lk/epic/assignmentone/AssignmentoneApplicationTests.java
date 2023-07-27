5664a749d637b3fe5bd363c14da511bd09017218
AbhiWPA <abhiashinsa770@gmail.com>
2023-07-26 22:26:50 -0700

    initial commit

---
+++ src/test/java/lk/epic/assignmentone/AssignmentoneApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.epic.assignmentone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
